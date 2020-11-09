efbd847de079c0d05c8a32784a3b659b0db6c145
SobakaWWW <56973435+SobakaWWW@users.noreply.github.com>
2020-11-09 16:26:54 +0300

    Add files via upload

---
+++ Assets/ClickScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+    public Camera mainCamera;
+    RaycastHit hit;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("entered");
+         
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                ShaderBehaviour script = hit.collider.gameObject.GetComponent<ShaderBehaviour>();
+                if (null != script)
+                    script.PaintOn(hit.textureCoord,  (Vector3)Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            }
+        }
+    }
+}
---
+++ Assets/ShaderBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderBehaviour : MonoBehaviour
+{
+    private Material m_material;
+    private Texture2D m_texture;
+    
+    private const int textureHeight = 512;//500;//512;//256;
+    private const int textureWidth = 512;//500;//512;//256;
+    private readonly Color c_color = new Color(0, 0, 0, 0);
+ 
+    
+    private bool isEnabled = false;
+
+    void Start()
+    {
+           Renderer renderer = GetComponent<Renderer>();
+     
+        if (null != renderer)
+        {
+            foreach (Material material in renderer.materials)
+            {
+                if (material.shader.name == "Custom/TestShader")
+                {
+                    material.EnableKeyword ("_NORMALMAP");
+                    material.EnableKeyword ("_METALLICGLOSSMAP");
+                    m_material = material;
+
+                    break;
+                }
+            }
+
+            if (null != m_material)
+            {
+               m_texture = new Texture2D(textureWidth, textureHeight);
+               for (int x = 0; x < textureWidth; ++x)
+                   for (int y = 0; y < textureHeight; ++y)
+                       m_texture.SetPixel(x, y, c_color);
+                m_texture.Apply();
+                
+              m_material.SetTexture("_DynamicTex", m_texture);
+            
+               isEnabled = true;
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PaintOn(Vector2 textureCoord, Vector2 mousePos)
+    {
+        if (isEnabled)
+        {
+          Debug.Log(textureCoord);
+        int xx = (int)(textureCoord.x * textureWidth);
+        int y = (int)(textureCoord.y * textureHeight); 
+        DrawCircle ((new Color(0.0f/255.0f,110.0f/255.0f,110.0f/255.0f,255.0f/255.0f)),xx,y,20); 
+        }
+    }
+
+        void DrawCircle(Color color, int x, int y, int radius = 3)
+{
+     Debug.Log("entered");
+     float rSquared = radius * radius;
+        for (int u = x - radius; u < x + radius + 1; u++)
+       for (int v = y - radius; v < y + radius + 1; v++)
+           if ((x - u) * (x - u) + (y - v) * (y - v) < rSquared)
+               m_texture.SetPixel(u, v, color);
+  
+    m_texture.Apply();
+
+      Debug.Log("ended");
+ 
+}
+
+}
